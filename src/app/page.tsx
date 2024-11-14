58857d885a5eea36ec0580b8aa0b9e3bd3c4da7d
napatnill <napatnill.nn@gmail.com>
2024-11-15 06:01:42 +0700

    feat: simple landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import AppLayout from "@/components/layout/app-layout";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+
 export default function Home() {
   return (
-    <div>
-      Campground
-    </div>
+    <AppLayout>
+      <div className="w-full flex flex-col flex-1 overflow-hidden">
+          <header className="flex items-center justify-between px-6 py-4 border-b">
+            <div className="flex items-center gap-4">
+              <SidebarTrigger />
+              <h1 className="text-2xl font-bold">Welcome to Campground Website</h1>
+            </div>
+          </header>
+          <main className="flex-1 overflow-auto p-6">
+            <h2 className="text-3xl font-bold mb-4">Landing Page Content</h2>
+            <p className="mb-4">
+              Explore the ultimate destination for outdoor enthusiasts—discover top-rated campgrounds, plan your adventure, and experience nature like never before on Campground Website!
+            </p>
+          </main>
+        </div>
+    </AppLayout>
   );
 }
